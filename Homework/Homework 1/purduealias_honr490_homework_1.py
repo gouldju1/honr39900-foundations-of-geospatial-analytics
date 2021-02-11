9efc2db0f433c90c16cba85741128f1802112eac
Justin Gould <gouldju1@gmail.com>
2021-02-10 19:27:16 -0500

    Add boilerplate homework files

--- Homework/Homework 1/purduealias_honr490_homework_1.py
+++ Homework/Homework 1/purduealias_honr490_homework_1.py
@@ -6,6 +6,7 @@ Date: INSERT DATE
 #Required Packages
 import pandas as pd
 import numpy as np
+import datetime
 from pandasql import *
 
 #Set up pandasql
@@ -14,3 +15,5 @@ pysqldf = lambda q: sqldf(q, globals())
 #Load Datasets
 meat = load_meat()
 births = load_births()
+
+#START HOMEWORK
